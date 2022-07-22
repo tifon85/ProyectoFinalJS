bc03233a5a97e2e3f05e898d16c153671ede2ef0
tifon85 <nico.ten85@gmail.com>
2022-07-22 03:15:28 -0300

    Desafio Eventos

--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
 
-
-
-
+let carritoDeCompras = []
 
 const contenedorProductos = document.getElementById('contenedor-productos');
 
@@ -9,25 +7,37 @@ mostrarProductos(stockProductos)
 
 //logica Ecommerce
 function mostrarProductos(array){
-    contenedorProductos.innerHTML= ""
+    //contenedorProductos.innerHTML= ""
     
    array.forEach(item => {
 
        let div = document.createElement('div')
        div.classList.add('producto')
-    //    div.setAttribute('class', 'producto')
-    //    div.className = 'producto'
-    div.innerHTML += `
-                    <div class="card" style="width: 18rem;">
-                        <img src="${item.imagen}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">${item.nombre}</h5>
-                            <p class="card-text">${item.descripcion}</p>
-                            <p class="card-text">$${item.precio}</p>
+        div.innerHTML += `
+                        <div class="card" style="width: 18rem;">
+                            <img src="${item.imagen}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">${item.nombre}</h5>
+                                <p class="card-text">${item.descripcion}</p>
+                                <p class="card-text">$${item.precio}</p>
+                            </div>
                         </div>
-                    </div>
-    `
-    contenedorProductos.appendChild(div)
+        `
+        contenedorProductos.appendChild(div)
+
+        let btnAgregar = document.getElementById(`agregar${item.id}`)
+
+        btnAgregar.addEventListener('click',()=>{
+           agregarAlCarrito(item.id)
+        })
 
    })
+}
+
+function agregarAlCarrito(id) {
+    let productoAgregar = stockProductos.find(elemento => elemento.id == id)
+    
+    productoAgregar.cantidad = 1
+        
+    carritoDeCompras.push(productoAgregar)
 }
