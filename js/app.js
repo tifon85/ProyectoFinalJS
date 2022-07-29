7ceda50c4746e5ca373f39acfebe028d36b69184
tifon85 <nico.ten85@gmail.com>
2022-07-29 02:45:41 -0300

    Segunda Entrega JS

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 let carritoDeCompras = []
 
 const contenedorProductos = document.getElementById('contenedor-productos');
+const contenedorCarrito = document.getElementById('carrito-contenedor');
 
 mostrarProductos(stockProductos)
 
@@ -36,11 +37,52 @@ function mostrarProductos(array){
 }
 
 function agregarAlCarrito(id) {
-    let productoAgregar = stockProductos.find(elemento => elemento.id == id)
-    
-    productoAgregar.cantidad = 1
-        
-    carritoDeCompras.push(productoAgregar)
 
-    console.log(carritoDeCompras)
+    let yaEsta = carritoDeCompras.find(elemento=> elemento.id == id)
+    if(yaEsta){
+        yaEsta.cantidad = yaEsta.cantidad + 1
+        document.getElementById(`und${yaEsta.id}`).innerHTML =` <p id=und${yaEsta.id}>Und:${yaEsta.cantidad}</p>`
+    }else{
+       let productoAgregar = stockProductos.find(elemento => elemento.id == id)
+    
+        productoAgregar.cantidad = 1
+        
+        carritoDeCompras.push(productoAgregar)
+
+        mostrarCarrito(productoAgregar) 
+    }
+
+    localStorage.setItem('carrito', JSON.stringify(carritoDeCompras))
+    
+}
+
+function mostrarCarrito(productoAgregar) {
+
+    let div = document.createElement('div')
+    div.className = 'productoEnCarrito'
+    div.innerHTML=`
+                    <p>${productoAgregar.nombre}</p>
+                    <p>Precio: $${productoAgregar.precio}</p>
+                    <p id="und${productoAgregar.id}">Und:${productoAgregar.cantidad}</p>
+                    <button id="eliminar${productoAgregar.id}" class="boton-eliminar"><i class="fas fa-trash-alt">Quitar</i></button>
+    `
+    contenedorCarrito.appendChild(div)
+
+    let btnEliminar = document.getElementById(`eliminar${productoAgregar.id}`)
+
+    btnEliminar.addEventListener('click',()=>{
+        if(productoAgregar.cantidad == 1){
+           btnEliminar.parentElement.remove()
+            carritoDeCompras = carritoDeCompras.filter(item=> item.id != productoAgregar.id)
+            localStorage.setItem('carrito', JSON.stringify(carritoDeCompras))
+        }else{
+            productoAgregar.cantidad = productoAgregar.cantidad - 1
+            document.getElementById(`und${productoAgregar.id}`).innerHTML =` <p id=und${productoAgregar.id}>Und:${productoAgregar.cantidad}</p>`
+            localStorage.setItem('carrito', JSON.stringify(carritoDeCompras))
+            }
+        
+
+    })
+
+
 }
